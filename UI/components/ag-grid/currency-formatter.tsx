49f1c68b9658515b5210cc2f98378fe70e23942e
Ryan <ryan-s-miller@hotmail.com>
2025-05-26 16:54:04 -0500

    Minor fixes

--- UI/components/ag-grid/currency-formatter.tsx
+++ UI/components/ag-grid/currency-formatter.tsx
@@ -2,5 +2,5 @@ import {   ValueFormatterParams } from "ag-grid-community";
 
 export function CurrencyFormatter(params: ValueFormatterParams)
 {
-    return "$" + Math.floor(params.value).toLocaleString();
+    return "$" + Math.floor(params.value).toFixed(2).toLocaleString();
 }
